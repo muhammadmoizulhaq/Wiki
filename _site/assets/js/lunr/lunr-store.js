b46510c9ad41363b75079bc04f32f18046fe89b3
muhammadmoizulhaq <moizulhaq914@gmail.com>
2023-04-18 21:40:49 +0500

    Changes set

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "BP vs C++ Unreal Engine",
+        "excerpt":"Comparison b/w Blueprints and C++ Blueprints C plus plus No Codding required Must have basic information about programming Pros Pros Bit slower Greater flexibility Quicker to create new game Execute much quicker Best for Game Designer Best for Game Programmers Cons Cons Messier It is easier to make mistakes Harder...","categories": ["devdoc","comparison"],
+        "tags": ["development","gameplay","programming","documentation","unreal engine","c++","blueprint","cplusplus"],
+        "url": "/BP-vs-cpp-unreal-engine/",
+        "teaser": null
+      }]
